bda4f5b0958226c6bbc693721507e7b5c7322d3a
shivam <shi845285@gmail.com>
2024-01-08 01:31:40 +0530

    testimonal added

--- src/components/Testimonal/Testimonal.js
+++ src/components/Testimonal/Testimonal.js
@@ -6,7 +6,13 @@ import "slick-carousel/slick/slick-theme.css";
 import { Col, Container, Row } from "react-bootstrap";
 import "./../../styles/Testimonalcss.css";
 // import { FaBeer, FaChevronLeft } from "react-icons/fa";
-import anthonyreedy from "./../../assests/testimonal/anthonygrouped.svg";
+import anthonyreedy from "./../../assests/testimonal/reddy.svg";
+import playbtn from "./../../assests/testimonal/play.svg";
+import university from "./../../assests/testimonal/University_of_Europe.png";
+
+
+import { FaPlayCircle } from 'react-icons/fa';
+
 function Success() {
   const [sliderRef, setSliderRef] = useState(null);
 
@@ -53,46 +59,21 @@ function Success() {
       pricingText: "USD 50/Day",
       features: ["Free Wifi", "Free breakfast"],
     },
-    // {
-    //   imageSrc:
-    //     "https://images.unsplash.com/photo-1616940844649-535215ae4eb1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
-    //   title: "Deluxe Room",
-    //   description: "Lorem ipsum dolor sit amet, consectur dolori",
-    //   pricingText: "USD 80/Day",
-    //   features: ["Free Wifi", "Free breakfast"],
-    // },
-    // {
-    //   imageSrc:
-    //     "https://images.unsplash.com/photo-1559508551-44bff1de756b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80",
-    //   title: "Studio Room",
-    //   description: "Lorem ipsum dolor sit amet, consectur dolori",
-    //   pricingText: "USD 50/Day",
-    //   features: ["Free Wifi", "Free breakfast"],
-    // },
-    // {
-    //   imageSrc:
-    //     "https://images.unsplash.com/photo-1559508551-44bff1de756b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80",
-    //   title: "Studio Room",
-    //   description: "Lorem ipsum dolor sit amet, consectur dolori",
-    //   pricingText: "USD 50/Day",
-    //   features: ["Free Wifi", "Free breakfast"],
-    // },
-    // {
-    //   imageSrc:
-    //     "https://images.unsplash.com/photo-1559508551-44bff1de756b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80",
-    //   title: "Studio Room",
-    //   description: "Lorem ipsum dolor sit amet, consectur dolori",
-    //   pricingText: "USD 50/Day",
-    //   features: ["Free Wifi", "Free breakfast"],
-    // },
-    // {
-    //   imageSrc:
-    //     "https://images.unsplash.com/photo-1559508551-44bff1de756b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80",
-    //   title: "Studio Room",
-    //   description: "Lorem ipsum dolor sit amet, consectur dolori",
-    //   pricingText: "USD 50/Day",
-    //   features: ["Free Wifi", "Free breakfast"],
-    // },
+    {
+      imageSrc: anthonyreedy,
+      title: "Studio Room",
+      description: "Lorem ipsum dolor sit amet, consectur dolori",
+      pricingText: "USD 50/Day",
+      features: ["Free Wifi", "Free breakfast"],
+    },
+    {
+      imageSrc: anthonyreedy,
+      title: "Studio Room",
+      description: "Lorem ipsum dolor sit amet, consectur dolori",
+      pricingText: "USD 50/Day",
+      features: ["Free Wifi", "Free breakfast"],
+    },
+
   ];
 
   return (
@@ -105,7 +86,7 @@ function Success() {
             </h3>
           </div>
           <div className="h-full min-w-full">
-            <div className="mb-4 flex justify-end md:mr-20 mr-2">
+            <div className="mb-2 flex justify-end md:mr-20 mr-2">
               <button
                 onClick={sliderRef?.slickPrev}
                 className="bg-orange-500 p-3 rounded-full text-white hover:text-black"
@@ -119,24 +100,50 @@ function Success() {
                 <FaChevronRight className="text__20" />
               </button>
             </div>
-            <Row>
+         
             <Slider ref={setSliderRef} {...sliderSettings}>
               {hotelCards.map((card, index) => (
-                <Col className="m-auto">
-                <div key={index}>
-                  <div className="testimonal border">
+            <>
+                  <div className="testimonal">
+                    <div className=" h-full ">
+                    <div className="relative text-left">
+                      <div className="px-3 py-3 ">
+                    <p className="text-base text-left text-[#000000]" style={{lineHeight:"1.10rem"}}>My entire application process with Germanywale has been 
+                    extremely smooth. Everyone in the team is exceptional and very helpful.   I have zero complaints. I would recommend   
+                    </p>
+                     
+                     <p className="text-base text-left w-64 text-[#000000]" style={{lineHeight:"1.10rem"}}>Germanywale to any student who is looking to go to Germany to make their
+                      study-abroad <br/>dreams come true.</p>
+                      <div className="ml-28"> <button className="namebtn mt-3">Anthony Reddy</button></div>
+                      <p className="text-xs text-left text-[#000000] w-32">Master of Business Administration</p>
+                     <img src={university} alt="university_logo" height={70} width={65} className=""/>
+                     </div>
+                     <div className="firstvector">
+                      
+                      </div>
+                      <div className="secondvector">
+                        
+                        </div>
+                    </div>
+                  
+                    </div>
+                    <div className="view text-lg"> 
+                     {/* <FaPlayCircle className="text-xl"/> */}
+                     <img src={playbtn} alt="playbtn"/>
+                      &nbsp;Watch Story
+                    </div>
+                  </div>
+                
+                  {/* <div className="absolute w-96 h-auto" style={{bottom:"53px",zIndex:"0"}}>
                     <img
                       src={card.imageSrc}
                       alt={card.title}
-                      className="h-full rounded-xl w-full object-fill md:w-4/5"
+                      className="h-full w-full"
                     />
-                    hiii hell
-                  </div>
-                </div>
-                </Col>
+                  </div> */}
+                  </>
               ))}
             </Slider>
-            </Row>
           </div>
         </Container>
       </section>
