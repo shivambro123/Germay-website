b6b2a8e653572b31bba9b2d1542dcb4f9c3af349
shivam <shi845285@gmail.com>
2024-01-07 13:00:45 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Landingpage from "./pages/Landingpage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Landingpage />
+      <Router>
+
+        <Routes>
+          <Route path="/" element={<Landingpage/>}/>
+          <Route path="/Germay-website/" element={<Landingpage/>}/>
+        </Routes>
+      </Router>
+      {/* <Landingpage /> */}
     </div>
   );
 }
